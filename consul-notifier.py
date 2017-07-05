0b121c8b7ebe8e280aefe3a6da87c9e26afb17d5
codelusion <ash@aisaac.io>
2017-07-05 15:49:07 -0500

    Register/De-register bugfix

--- consul-notifier.py
+++ consul-notifier.py
@@ -30,7 +30,7 @@ def setup_logging(verbose=False):
 
 class Service(object):
 
-    def __init__(self, cli, con, name, service):
+    def __init__(self, docker_client, consul_instance, name, service):
 
         # http://gliderlabs.com/blog/2015/04/14/docker-events-explained/
         self.status_map = {
@@ -41,8 +41,8 @@ class Service(object):
             "deregister": "deregister"
         }
 
-        self.con = con
-        self.cli = cli
+        self.consul_instance = consul_instance
+        self.docker_client = docker_client
         self.name = name
         self.service = service
 
@@ -64,7 +64,7 @@ class Service(object):
         if (action in self.status_map and
                 hasattr(self, self.status_map[action])):
 
-            self.container = self.cli.inspect_container(self.name)
+            self.container = self.docker_client.inspect_container(self.name)
             self.env = self.container['Config']['Env']
             self.hostname = self.container['Config']['Hostname']
             self.port = self.get_port(None)
@@ -91,14 +91,25 @@ class Service(object):
             self.container_id,
             self.port))
 
-        res = self.con.agent.service.register(
-            self.service,
-            service_id=self.container_id,
-            port=int(self.port))
+        for node in self.get_swarm_nodes():
+            res = self.consul_instance.agent.service.register(
+                self.service,
+                address=node,
+                check=self.get_service_health_check(node),
+                service_id=self.container_id,
+                port=int(self.port))
 
-        if not res:
-            logger.error("Failed to register service")
-            sys.exit(1)
+            if not res:
+                logger.error("Failed to register service at node: {0}".format(node))
+                sys.exit(1)
+
+    def get_swarm_nodes(self):
+        # self.docker_client.swarm_reload()
+        nodes = self.docker_client.nodes.list()
+        print(json.dumps(nodes, sort_keys=True, indent=4))
+        return [node.get('ip', 'ip') for node in nodes]
+
+
 
     def deregister(self):
         if not self.port:
@@ -111,7 +122,7 @@ class Service(object):
             self.service,
             self.container_id))
 
-        res = self.con.agent.service.deregister(service_id=self.container_id)
+        res = self.consul_instance.agent.service.deregister(service_id=self.container_id)
 
         if not res:
             logger.error("Failed to de-register service")
@@ -145,7 +156,7 @@ Register / De-register services manually or via Docker Daemon event stream
     return args
 
 
-def stream(cli, con):
+def stream(docker_client, consul_instance):
     """
     Connect to the docker daemon and listen for events
     possible events are:
@@ -180,7 +191,7 @@ def stream(cli, con):
     """
 
     # start listening for new events
-    for event in cli.events(decode=True):
+    for event in docker_client.events(decode=True):
 
         service_key = 'com.docker.swarm.service.name'
 
@@ -196,7 +207,7 @@ def stream(cli, con):
         print (json.dumps(event, sort_keys=True, indent=4))
         print("-" * 80)
 
-        s = Service(cli, con, name, service)
+        s = Service(docker_client, consul_instance, name, service)
         s.handle(action)
 
 
@@ -204,23 +215,24 @@ def main():
     """
         Register / De-register containers that have
         CONSUL_SERVICE_PORT env variable defined
+        if specified, CONSUL_ADDR refers to consul instance
     """
     args = handler_args()
     setup_logging(args.verbose)
 
     # create a docker client object that talks to the local docker daemon
-    cli = docker.Client(base_url='unix://var/run/docker.sock')
+    docker_client = docker.Client(base_url='unix://var/run/docker.sock')
 
     consul_host = os.environ.get('CONSUL_ADDR', '127.0.0.1')
     logger.info("Consul Host: {0}".format(consul_host))
-    con = consul.Consul(host=consul_host)
+    consul_instance = consul.Consul(host=consul_host)
 
     logger.info("Consul notifier processing {0}".format(args.action))
 
     if args.action == 'stream':
-        stream(cli, con)
+        stream(docker_client, consul_instance)
     elif args.action in ['register', 'deregister']:
-        s = Service(cli, con, args.name)
+        s = Service(docker_client, consul_instance, args.name)
         s.handle(args.action)
     else:
         logger.error("Unknown action {0}".format(args.action))
