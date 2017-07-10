44a8ddfba2c9da696249eaa44fa953d91eb5c91c
codelusion <ash@aisaac.io>
2017-07-10 08:57:57 -0500

    Register all nodes with health check

--- consul-notifier.py
+++ consul-notifier.py
@@ -4,21 +4,22 @@ import json
 import sys
 import logging
 import docker
+from docker import errors
 import consul
 import os
 
 logger = logging.getLogger(__name__)
 args = None
 
+
 # docker run -v /var/run/docker.sock:/var/run/docker.sock consul-notifier
 
 
 def setup_logging(verbose=False):
-    '''
+    """
     Setup logging
-
     :param verbose: bool - Enable verbose debug mode
-    '''
+    """
 
     ch = logging.StreamHandler()
     ch.setFormatter(logging.Formatter('[%(asctime)s] %(message)s'))
@@ -29,11 +30,11 @@ def setup_logging(verbose=False):
 
 
 class Service(object):
-
     def __init__(self, docker_client, consul_instance, name, service):
 
         # http://gliderlabs.com/blog/2015/04/14/docker-events-explained/
         self.status_map = {
+            "kill": "deregister",
             "stop": "deregister",
             "die": "deregister",
             "start": "register",
@@ -45,33 +46,41 @@ class Service(object):
         self.docker_client = docker_client
         self.name = name
         self.service = service
+        self.container = None
+        self.svc_spec = {}
 
-    def get_port(self, default):
-        for nv in self.env:
+    def get_env(self, env_key):
+        for nv in self.svc_spec['env']:
             n, v = str(nv).split('=')
-            if n == 'CONSUL_SERVICE_PORT':
+            if n == env_key:
                 return v
-
-        return default
 
     def get_id(self):
         return "{0}:{1}:{2}".format(
-            self.hostname,
-            self.container_name,
-            self.port)
+            self.svc_spec['hostname'],
+            self.svc_spec['container_name'],
+            self.svc_spec['port'])
 
     def handle(self, action):
         if (action in self.status_map and
                 hasattr(self, self.status_map[action])):
 
-            self.container = self.docker_client.inspect_container(self.name)
-            self.env = self.container['Config']['Env']
-            self.hostname = self.container['Config']['Hostname']
-            self.port = self.get_port(None)
+            try:
+                self.container = self.docker_client.inspect_container(self.name)
+            except errors.NotFound:
+                logger.warning("NotFound: Cannot handle {0} on Container {1}".format(self.name, action))
+                return
+
+            self.svc_spec['env'] = self.container['Config']['Env']
+            self.svc_spec['hostname'] = self.container['Config']['Hostname']
+            self.svc_spec['port'] = self.get_env('CONSUL_SERVICE_PORT') or None
+            self.svc_spec['health_check'] = self.get_env('CONSUL_HEALTH_CHECK') or ''
+            self.svc_spec['health_check_interval'] = self.get_env('CONSUL_HEALTH_INTERVAL') or 10
+            self.svc_spec['health_check_ssl'] = self.get_env('CONSUL_HEALTH_SSL') or False
 
             # Strip the leading slash
-            self.container_name = self.container['Name'][1:]
-            self.container_id = self.get_id()
+            self.svc_spec['container_name'] = self.container['Name'][1:]
+            self.svc_spec['container_id'] = self.get_id()
 
             if args.verbose:
                 print(json.dumps(self.container, sort_keys=True, indent=4))
@@ -81,38 +90,44 @@ class Service(object):
             logger.warning("Ignoring action {0}".format(action))
 
     def register(self):
-        if not self.port:
+        if not self.svc_spec['port']:
             logger.info(
                 "Skipping registration of {0} not port defined".format(
                     self.service))
+            return
 
         logger.info("Registering {0} {1} port {2}".format(
             self.service,
-            self.container_id,
-            self.port))
+            self.svc_spec['container_id'],
+            self.svc_spec['port']))
 
-        for node in self.get_swarm_nodes():
+        print(json.dumps(self.svc_spec, sort_keys=True, indent=4))
+
+        for node_addr in self.get_swarm_nodes_addr():
             res = self.consul_instance.agent.service.register(
                 self.service,
-                address=node,
-                check=self.get_service_health_check(node),
-                service_id=self.container_id,
-                port=int(self.port))
+                address=node_addr,
+                check=consul.Check.http(self.get_health_check_url(node_addr), self.svc_spec['health_check_interval']),
+                service_id=self.svc_spec['container_id'],
+                port=int(self.svc_spec['port']))
 
             if not res:
-                logger.error("Failed to register service at node: {0}".format(node))
-                sys.exit(1)
+                logger.error("Failed to register service at node: {0}".format(node_addr))
 
-    def get_swarm_nodes(self):
-        # self.docker_client.swarm_reload()
-        nodes = self.docker_client.nodes.list()
+    def get_swarm_nodes_addr(self):
+        nodes = self.docker_client.nodes()
         print(json.dumps(nodes, sort_keys=True, indent=4))
-        return [node.get('ip', 'ip') for node in nodes]
+        return [node['Status']['Addr'] for node in nodes]
 
-
+    def get_health_check_url(self, node_addr):
+        proto = 'https' if self.svc_spec['health_check_ssl'] else 'http'
+        if not self.svc_spec['health_check'] or self.svc_spec['health_check'] == '/':
+            return "%s://%s:%s/" % (proto, node_addr, self.svc_spec['port'])
+        else:
+            return "%s://%s:%s%s" % (proto, node_addr, self.svc_spec['port'], self.svc_spec['health_check'])
 
     def deregister(self):
-        if not self.port:
+        if not self.svc_spec['port']:
             logger.info(
                 "Skipping de-registration of {0} not port defined".format(
                     self.service))
@@ -120,17 +135,16 @@ class Service(object):
 
         logger.info("De-registering {0} {1}".format(
             self.service,
-            self.container_id))
+            self.svc_spec['container_id']))
 
-        res = self.consul_instance.agent.service.deregister(service_id=self.container_id)
+        res = self.consul_instance.agent.service.deregister(service_id=self.svc_spec['container_id'])
 
         if not res:
             logger.error("Failed to de-register service")
-            sys.exit(1)
+
 
 
 def handler_args():
-
     global args
 
     help_text = '''
@@ -163,31 +177,6 @@ def stream(docker_client, consul_instance):
         attach, commit, copy, create, destroy, die
         exec_create, exec_start, export, kill, oom, pause,
         rename, resize, restart, start, stop, top, unpause, update
-
-        Example start dict
-        {
-            'status': 'start',
-            'timeNano': 1471291489261243761,
-            'from': 'lucj/demo-www:1.0',
-            'Actor': {
-                'Attributes': {
-                    'com.docker.swarm.task': '',
-                    'name': 'www.8.e7nt85x8f6tic0tiow5wzv5z8',
-                    'com.docker.swarm.node.id': '2wzelo3sj0oowbuo2jxc9jcje',
-                    'image': 'lucj/demo-www:1.0',
-                    'com.docker.swarm.service.id': '5m24m676zm1q6tjdte2o06ieb',
-                    'com.docker.swarm.task.name': 'www.8',
-                    'com.docker.swarm.service.name': 'www',
-                    'com.docker.swarm.task.id': 'e7nt85x8f6tic0tiow5wzv5z8'
-                },
-                'ID': u'8217cb8565dd774f316c3e51b0f88551e3337edffb087f28db75fb7126160641'
-            },
-            'time': 1471291489,
-            'Action': u'start',
-            'Type': 'container',
-            'id': '8217cb8565dd774f316c3e51b0f88551e3337edffb087f28db75fb7126160641'
-        }
-
     """
 
     # start listening for new events
@@ -232,11 +221,10 @@ def main():
     if args.action == 'stream':
         stream(docker_client, consul_instance)
     elif args.action in ['register', 'deregister']:
-        s = Service(docker_client, consul_instance, args.name)
+        s = Service(docker_client, consul_instance, args.name, None)
         s.handle(args.action)
     else:
         logger.error("Unknown action {0}".format(args.action))
-        sys.exit(1)
 
 
 if __name__ == '__main__':
